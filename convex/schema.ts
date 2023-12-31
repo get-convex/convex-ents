3dfc39e52033abaece54369327d19715c9d8e491
Michal Srb <srb@convex.dev>
2023-12-31 05:56:10 -0800

    Upgrade ts in vain attempt to fix the inverse issue

--- convex/schema.ts
+++ convex/schema.ts
@@ -8,10 +8,6 @@ import { v } from "convex/values";
 
 const schema = defineEntSchema(
   {
-    // messages: defineTable({
-    //   text: v.string(),
-    //   authorId: v.id("users"),
-    // }).index("authorId", ["authorId"]),
     messages: defineEnt({
       text: v.string(),
     })
@@ -26,7 +22,6 @@ const schema = defineEntSchema(
       .edges("messages")
       .edges("followers", "followees", { to: "users" })
       .edges("friends", { to: "users" }),
-    // .edges("friends", "users"),
 
     profiles: defineEnt({
       bio: v.string(),
@@ -35,26 +30,7 @@ const schema = defineEntSchema(
     tags: defineEnt({
       name: v.string(),
     }).edges("messages"),
-
-    documents: defineEnt({
-      fieldOne: v.string(),
-      fieldTwo: v.object({
-        subFieldOne: v.array(v.number()),
-      }),
-    }),
-    // This definition matches the example query and mutation code:
-    numbers: defineEnt({
-      value: v.number(),
-    }),
   },
-  // If you ever get an error about schema mismatch
-  // between your data and your schema, and you cannot
-  // change the schema to match the current data in your database,
-  // you can:
-  //  1. Use the dashboard to delete tables or individual documents
-  //     that are causing the error.
-  //  2. Change this option to `false` and make changes to the data
-  //     freely, ignoring the schema. Don't forget to change back to `true`!
   { schemaValidation: false }
 );
 
