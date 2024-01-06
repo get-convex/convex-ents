e3802c008a77b2bd11b7ffdf76965fa715f98d63
Michal Srb <srb@convex.dev>
2024-01-06 11:40:20 -0800

    Docs for has

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,8 +1,7 @@
-const path = require("path");
-
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
+  defaultShowCopyCode: true,
 });
 
 module.exports = withNextra();
