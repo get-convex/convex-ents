c93a31f500af0690ec001afceb35303c9f1a7c15
Michal Srb <srb@convex.dev>
2024-01-09 10:30:33 -0800

    Upgrade convex-helpers and add ctx types

--- test/convex/types.ts
+++ test/convex/types.ts
@@ -1,6 +1,11 @@
+import { CustomCtx } from "convex-helpers/server/customFunctions";
 import { GenericEnt, GenericEntWriter } from "../../src";
 import { DataModel, TableNames } from "./_generated/dataModel";
+import { mutation, query } from "./functions";
 import { entDefinitions } from "./schema";
+
+export type QueryCtx = CustomCtx<typeof query>;
+export type MutationCtx = CustomCtx<typeof mutation>;
 
 export type Ent<TableName extends TableNames> = GenericEnt<
   DataModel,
