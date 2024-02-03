72ab49cb97707a8ef18490b47ff988f44f1efa56
Michal Srb <srb@convex.dev>
2024-02-02 17:32:07 -0800

    Added pictures to schema

---
+++ docs/components/Graphic.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import { useConfig } from "nextra-theme-docs";
+
+export function Graphic({
+  src,
+  dark,
+  ...props
+}: React.ComponentProps<typeof Image> & {
+  dark: React.ComponentProps<typeof Image>["src"];
+}) {
+  return (
+    <>
+      <Image src={src} {...props} width={500} className="mx-auto dark:hidden" />
+      <Image
+        src={dark}
+        {...props}
+        width={500}
+        className="mx-auto hidden dark:block"
+      />
+    </>
+  );
+}
